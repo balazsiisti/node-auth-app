8c7a78a2397adf8dc4dc1353aab094500d031091
balazsiisti <istvan.balazsi@ibm.com>
2021-04-06 09:36:36 +0300

    Third

--- index.js
+++ index.js
@@ -3,36 +3,38 @@ const express = require("express"),
   exphbs = require('express-handlebars'),
   session = require('express-session'),
   path = require('path');
+  
+  
+function authFunction(param) {  
+  authApp.use(session({
+    secret: 'keyboard cat',
+  }))
+  
+  authApp.engine('handlebars', exphbs());
+  authApp.set('view engine', 'handlebars');
+  authApp.set('views', path.join(__dirname, 'templates'));
+  authApp.get('/login', function (req, res) {
+    if (req.session.user) {
+      res.redirect('/')
+    } else {
+      res.render(param.loginPage);
+    }
+  });
+  
+  authApp.get('/gohome', function (req, res) {
+    req.session.user = req.query.fname;
+    res.redirect('/');
+  });
+  
+  authApp.get('/', (req, res, next) => {
+    console.log(req.test);
+    if (req.session.user) {
+      next();
+    } else {
+      res.redirect('/login')
+    }
+  });
+  return authApp;
+}
 
-authApp.use(session({
-  secret: 'keyboard cat',
-}))
-
-authApp.engine('handlebars', exphbs());
-authApp.set('view engine', 'handlebars');
-authApp.set('views', path.join(__dirname, 'templates'));
-
-authApp.get('/login', function (req, res) {
-  if (req.session.user) {
-    res.redirect('/')
-  } else {
-    res.render('home');
-  }
-});
-
-authApp.get('/gohome', function (req, res) {
-  console.log('%%%%%%%%%%%%%%%%%', req.query);
-  req.session.user = req.query.fname;
-  res.redirect('/');
-});
-
-authApp.get('/', (req, res, next) => {
-  console.log(req.test);
-  if (req.session.user) {
-    next();
-  } else {
-    res.redirect('/login')
-  }
-});
-
-module.exports = authApp;
+module.exports = authFunction;
